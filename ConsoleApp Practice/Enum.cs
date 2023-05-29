0961d14e936ed6bf946ba2bf8b9bc1a72f539929
AhmedMushtaq-GitHub <ahmed@AHMEDMUSHTAQ>
2023-05-29 18:23:43 +0500

    updated 29 05 2023

--- ConsoleApp Practice/Enum.cs
+++ ConsoleApp Practice/Enum.cs
@@ -1,9 +1,34 @@
 ﻿namespace ConsoleApp_Practice;
 
-enum Sizes
+/*enum Sizes
 {
     small = 999,  
     medium = 999, 
     large = 999,
     extra_large = 1299
+}*/
+
+
+/*enum DayOfWeek
+{
+    sunday,
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday
+}*/
+
+enum season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
+
+enum Operation
+{
+    addition, subtraction, multiplication, division
 }
--- ConsoleApp Practice/Program.cs
+++ ConsoleApp Practice/Program.cs
@@ -1,8 +1,9 @@
 ﻿
 
 
-using ConsoleApp_Practice;
-Console.WriteLine("\t Which Size of Shirt you want ");
+//using ConsoleApp_Practice;
+
+/*Console.WriteLine("\t Which Size of Shirt you want ");
 Console.WriteLine("1 : Small ");
 Console.WriteLine("2 : Medium ");
 Console.WriteLine("3 : Large ");
@@ -49,4 +50,114 @@ if (input2 == "yes")
     Console.WriteLine("Thanks for shoping your order is redy");
 }
 else
-    Console.WriteLine("Thnks for visit our shop");
+    Console.WriteLine("Thnks for visit our shop");*/
+
+
+/*Console.WriteLine("\t .........What day is Today ..........");
+Console.WriteLine("Select a number from 7 days off week it will show you the day name");
+int input =Convert.ToInt32(Console.ReadLine());
+DayOfWeek day1 = DayOfWeek.Sunday;
+DayOfWeek day2 = DayOfWeek.Monday;
+DayOfWeek day3 = DayOfWeek.Tuesday;
+DayOfWeek day4 = DayOfWeek.Wednesday;
+DayOfWeek day5 = DayOfWeek.Thursday;
+DayOfWeek day6 = DayOfWeek.Friday;
+DayOfWeek day7 = DayOfWeek.Saturday;
+
+switch (input)
+{
+    case 1:
+        Console.WriteLine("Today is Sunday");
+        break;
+    case 2:
+        Console.WriteLine("Today is Monday");
+        break;
+    case 3:
+        Console.WriteLine("Today is Tuesday");
+        break;
+        case 4:
+        Console.WriteLine("Today is Wednesday");
+        break;
+        case 5:
+        Console.WriteLine("Today is Thursday");
+        break;
+        case 6:
+        Console.WriteLine("Today is Friday");
+        break;
+        case 7:
+        Console.WriteLine("Today is Saturday");
+        break;
+    default:
+        Console.WriteLine("You enter a number out off week days");
+        break;
+}
+
+
+*/
+
+/*
+DayOfWeek days = DayOfWeek.Friday;
+Console.WriteLine($"Today is {days}");*/
+/*Type days = typeof(DayOfWeek);
+string[] DayOfWeek = Enum.GetNames(days);
+*/
+
+/*
+using ConsoleApp_Practice;
+
+
+
+Console.WriteLine("Enter Date to see whats the season is going on (yyyy-mm-dd)");
+DateTime input =Convert.ToDateTime( Console.ReadLine());
+int month = input.Month;
+
+if (month < 2 || month == 12)
+{
+    Console.WriteLine($"The season is going on {season.Winter}");
+}
+else if (month > 2 || month <= 5)
+{
+    Console.WriteLine($"The season is going on {season.Spring}");
+}
+else if (month > 6 || month <= 8)
+{
+    Console.WriteLine($"The season is going on {season.Summer}");
+}
+else
+{
+    Console.WriteLine($"The season is going on {season.Autumn}");
+}
+*/
+using ConsoleApp_Practice;
+
+
+Console.WriteLine("............Well Come Ahmed's Calculator..........");
+Console.Write(" Enter First value >>");
+int value1 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Enter First value >>");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The four basic operators.witch you want to use \n 1 - {Operation.addition} \n 2 - {Operation.subtraction} \n 3 - {Operation.multiplication} \n 4 - {Operation.division}");
+int userSlection =Convert.ToInt32( Console.ReadLine());
+
+if (userSlection == 1)
+{
+    Console.WriteLine($"Your result is {value1 + value2}");
+}
+else  if (userSlection == 2)
+{
+    Console.WriteLine($"Your result is {value1 - value2}");
+}
+else  if (userSlection == 3)
+{
+    Console.WriteLine($"Your result is {value1 * value2}");
+}
+else if (userSlection == 4)
+{
+    Console.WriteLine($"Your result is {value1 / value2}");
+
+}
+else 
+{
+    Console.WriteLine("you are Selecting a rong number");
+}
